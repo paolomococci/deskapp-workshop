de65a5acafc1dc13c9cb5c535083dfad196c9643
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-09-20 19:46:36 +0200

    Create MainView.kt

---
+++ webapp-empty-kotlin/src/main/kotlin/local/example/seed/view/MainView.kt
@@ -0,0 +1,49 @@
+/**
+ *
+ * Copyright 2020 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed following in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package local.example.seed.view
+
+import com.github.mvysny.karibudsl.v10.button
+import com.github.mvysny.karibudsl.v10.formLayout
+import com.github.mvysny.karibudsl.v10.onLeftClick
+import com.github.mvysny.karibudsl.v10.textField
+import com.vaadin.flow.component.html.Main
+import com.vaadin.flow.component.icon.Icon
+import com.vaadin.flow.component.icon.VaadinIcon
+import com.vaadin.flow.component.notification.Notification
+import com.vaadin.flow.component.textfield.TextField
+import com.vaadin.flow.router.Route
+
+@Route("")
+class MainView : Main() {
+
+    private lateinit var nameField: TextField
+    private lateinit var surnameField: TextField
+
+    init {
+        formLayout {
+            nameField = textField("name")
+            surnameField = textField("surname")
+        }
+        button("welcome", Icon(VaadinIcon.ALARM)) {
+            onLeftClick {
+                Notification.show("Welcome, ${nameField.value} ${surnameField.value}")
+            }
+        }
+    }
+}
