f12f3ac8b3f0dbff06f9a24a6bdf2aaa2ca900ae
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-09-21 23:27:30 +0200

    Update ApplicationShellConf.kt

--- webapp-empty-kotlin/src/main/kotlin/local/example/seed/conf/ApplicationShellConf.kt
+++ webapp-empty-kotlin/src/main/kotlin/local/example/seed/conf/ApplicationShellConf.kt
@@ -21,5 +21,5 @@ package local.example.seed.conf
 import com.vaadin.flow.component.page.AppShellConfigurator
 import com.vaadin.flow.server.PWA
 
-@PWA(name = "sample-empty", shortName = "sample")
+@PWA(name = "example-empty", shortName = "example")
 class ApplicationShellConf : AppShellConfigurator
