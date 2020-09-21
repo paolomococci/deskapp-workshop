d720566d9972387fa1a21ff6df8937ee2b982daf
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-09-21 23:24:35 +0200

    Create MainLayout.kt

---
+++ webapp-empty-kotlin/src/main/kotlin/local/example/seed/layout/MainLayout.kt
@@ -0,0 +1,64 @@
+/**
+ *
+ * Copyright 2020 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed following in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package local.example.seed.layout
+
+import com.vaadin.flow.component.Component
+import com.vaadin.flow.component.applayout.AppLayout
+import com.vaadin.flow.component.applayout.DrawerToggle
+import com.vaadin.flow.component.dependency.CssImport
+import com.vaadin.flow.component.html.H1
+import com.vaadin.flow.component.orderedlayout.FlexComponent
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout
+import com.vaadin.flow.component.orderedlayout.VerticalLayout
+import com.vaadin.flow.component.tabs.Tabs
+import com.vaadin.flow.theme.Theme
+import com.vaadin.flow.theme.lumo.Lumo
+
+
+@CssImport(value = "style.css")
+@Theme(value = Lumo::class, variant = Lumo.DARK)
+class MainLayout : AppLayout() {
+
+    private lateinit var menu: Tabs
+    private lateinit var title: H1
+
+    init {
+        primarySection = Section.DRAWER
+        addToNavbar(true)
+    }
+
+    fun createHeaderContent() : Component {
+        val horizontalLayout = HorizontalLayout()
+        horizontalLayout.setId("header")
+        horizontalLayout.themeList.set("dark", true)
+        horizontalLayout.setWidthFull()
+        horizontalLayout.isSpacing = false
+        horizontalLayout.alignItems = FlexComponent.Alignment.CENTER
+        horizontalLayout.add(DrawerToggle())
+        title = H1()
+        horizontalLayout.add(title)
+        return horizontalLayout
+    }
+
+    fun createDrawerContent() : Component {
+        val verticalLayout = VerticalLayout()
+        // TODO
+        return verticalLayout
+    }
+}
