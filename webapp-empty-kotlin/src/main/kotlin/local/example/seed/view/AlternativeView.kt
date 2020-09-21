4edef7293bda123f1e3c7c29597f1e22f3dd7d4a
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-09-21 23:44:03 +0200

    Update AlternativeView.kt

--- webapp-empty-kotlin/src/main/kotlin/local/example/seed/view/AlternativeView.kt
+++ webapp-empty-kotlin/src/main/kotlin/local/example/seed/view/AlternativeView.kt
@@ -24,9 +24,11 @@ import com.vaadin.flow.component.button.Button
 import com.vaadin.flow.component.dependency.CssImport
 import com.vaadin.flow.component.notification.Notification
 import com.vaadin.flow.component.textfield.TextField
+import com.vaadin.flow.router.PageTitle
 import com.vaadin.flow.router.Route
 
 @Route("alternative")
+@PageTitle("alternative")
 @CssImport(value = "style.css")
 class AlternativeView  : KComposite() {
 
