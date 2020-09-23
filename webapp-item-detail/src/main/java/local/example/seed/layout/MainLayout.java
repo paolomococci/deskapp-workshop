fbd5cc264aff6e71f305cdc01132165eb5f96131
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-09-23 23:20:11 +0200

    Update MainLayout.java

--- webapp-item-detail/src/main/java/local/example/seed/layout/MainLayout.java
+++ webapp-item-detail/src/main/java/local/example/seed/layout/MainLayout.java
@@ -18,5 +18,67 @@
 
 package local.example.seed.layout;
 
-public class MainLayout {
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.tabs.Tab;
+import com.vaadin.flow.component.tabs.Tabs;
+import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.lumo.Lumo;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
+
+@Theme(value = Lumo.class, variant = Lumo.DARK)
+public class MainLayout
+        extends AppLayout {
+
+    private final Tabs tabs;
+    private H1 title;
+
+    @Autowired
+    public MainLayout() {
+        tabs = null;
+    }
+
+    private Component createHeaderContent() {
+        // TODO
+        return null;
+    }
+
+    private Component createDrawerContent(Tabs tabs) {
+        // TODO
+        return null;
+    }
+
+    private Tabs createMenu() {
+        // TODO
+        return null;
+    }
+
+    private Component[] createMenuItems() {
+        // TODO
+        return new Component[0];
+    }
+
+    private Optional<Tab> getTabForComponent(Component component) {
+        // TODO
+        return null;
+    }
+
+    private static Tab createTab() {
+        // TODO
+        return null;
+    }
+
+    private String getCurrentPageTitle() {
+        // TODO
+        return null;
+    }
+
+    @Override
+    protected void afterNavigation() {
+        super.afterNavigation();
+        // TODO
+    }
 }
