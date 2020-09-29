57db2fd1643b1123841de49a8285b580f1f9d9d8
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-09-29 17:01:31 +0200

    Update AboutView.kt

--- webapp-empty-kotlin/src/main/kotlin/local/example/seed/view/AboutView.kt
+++ webapp-empty-kotlin/src/main/kotlin/local/example/seed/view/AboutView.kt
@@ -32,7 +32,7 @@ import local.example.seed.layout.MainLayout
 class AboutView : Div() {
 
     init {
-        addClassName("view")
+        addClassName("main-view")
         add(
                 VerticalLayout(
                         Label("the content of the about page has yet to be developed")
