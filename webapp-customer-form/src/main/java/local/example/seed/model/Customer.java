a33933d46ef689f1d29befd20219563cb6652ae2
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-09-30 20:12:34 +0200

    Update Customer.java

--- webapp-customer-form/src/main/java/local/example/seed/model/Customer.java
+++ webapp-customer-form/src/main/java/local/example/seed/model/Customer.java
@@ -18,5 +18,16 @@
 
 package local.example.seed.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Customer {
+    private String id;
+    private String name;
+    private String surname;
+    private String email;
 }
