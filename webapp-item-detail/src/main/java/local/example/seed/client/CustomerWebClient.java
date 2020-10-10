ec5518f3efa4b735e3337e8441f59f1008827cad
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-10-10 23:11:21 +0200

    Update CustomerWebClient.java

--- webapp-item-detail/src/main/java/local/example/seed/client/CustomerWebClient.java
+++ webapp-item-detail/src/main/java/local/example/seed/client/CustomerWebClient.java
@@ -18,53 +18,25 @@
 
 package local.example.seed.client;
 
-import io.netty.channel.ChannelOption;
-import io.netty.handler.timeout.ReadTimeoutHandler;
-import io.netty.handler.timeout.WriteTimeoutHandler;
 import local.example.seed.model.Customer;
-import org.springframework.http.HttpHeaders;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-import reactor.netty.http.client.HttpClient;
-import reactor.netty.tcp.TcpClient;
 
+import java.net.URI;
 import java.sql.Timestamp;
-import java.util.concurrent.TimeUnit;
 
 public class CustomerWebClient {
 
-    private final WebClient webClient;
+    private static final URI CUSTOMERS = URI.create("http://127.0.0.1:8080/customers");
 
-    public CustomerWebClient() {
-        TcpClient tcpClient = TcpClient.create()
-                .option(
-                        ChannelOption.CONNECT_TIMEOUT_MILLIS, 6000
-                )
-                .doOnConnected(
-                        connection -> {
-                            connection.addHandlerLast(
-                                    new ReadTimeoutHandler(6000, TimeUnit.MILLISECONDS));
-                            connection.addHandlerLast(
-                                    new WriteTimeoutHandler(6000, TimeUnit.MILLISECONDS));
-                        }
-                );
-        webClient = WebClient.builder()
-                .baseUrl("http://localhost:8080")
-                .clientConnector(
-                        new ReactorClientHttpConnector(HttpClient.from(tcpClient))
-                )
-                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                .build();
-    }
+    @Autowired
+    private WebClient webClient = WebClient.create(CUSTOMERS.getPath());
 
     public Mono<Customer> create(Customer customer) {
         return this.webClient
                 .post()
-                .uri("/customers")
                 .body(Mono.just(customer), Customer.class)
                 .retrieve()
                 .onStatus(
@@ -84,7 +56,7 @@ public class CustomerWebClient {
     public Mono<Customer> read(String id) {
         return this.webClient
                 .get()
-                .uri("/customers/"+id)
+                .uri("/"+id)
                 .retrieve()
                 .onStatus(
                         httpStatus -> HttpStatus.NOT_FOUND.equals(httpStatus),
@@ -110,28 +82,10 @@ public class CustomerWebClient {
                 .onErrorResume(exception -> Mono.empty());
     }
 
-    public Flux<Customer> readAll() {
-        return this.webClient
-                .get()
-                .uri("/customers")
-                .retrieve()
-                .onStatus(
-                        HttpStatus::isError,
-                        clientResponse -> Mono.empty()
-                )
-                .bodyToFlux(Customer.class)
-                .doOnError(exception -> {
-                    Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-                    System.out.println(timestamp +
-                            " ERROR: --- Connection refused occurred during a request read all customers, probably the host is down! ---");
-                })
-                .onErrorResume(exception -> Mono.empty());
-    }
-
     public Mono<Customer> update(Customer customer, String id) {
         return this.webClient
                 .put()
-                .uri("/customers/"+id)
+                .uri("/"+id)
                 .body(Mono.just(customer), Customer.class)
                 .retrieve()
                 .onStatus(
@@ -153,7 +107,7 @@ public class CustomerWebClient {
     public Mono<Customer> partialUpdate(Customer customer, String id) {
         return this.webClient
                 .patch()
-                .uri("/customers/"+id)
+                .uri("/"+id)
                 .body(Mono.just(customer), Customer.class)
                 .retrieve()
                 .onStatus(
@@ -175,7 +129,7 @@ public class CustomerWebClient {
     public Mono<Void> delete(String id) {
         return this.webClient
                 .delete()
-                .uri("/customers/"+id)
+                .uri("/"+id)
                 .retrieve()
                 .onStatus(
                         httpStatus -> HttpStatus.NOT_FOUND.equals(httpStatus),
