fa25bbc9cf8cdddb8a504c9e08961f6159e943f0
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-10-11 17:52:29 +0200

    Update Address.java

--- webapp-item-detail/src/main/java/local/example/seed/model/Address.java
+++ webapp-item-detail/src/main/java/local/example/seed/model/Address.java
@@ -26,7 +26,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Address {
-    private String id;
     private String country;
     private String city;
     private String street;
