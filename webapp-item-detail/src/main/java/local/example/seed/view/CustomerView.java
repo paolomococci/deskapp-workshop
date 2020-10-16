744188c5ae5f3db7a60c9dfd471c104c85bcf9a8
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-10-16 08:29:50 +0200

    Update CustomerView.java

--- webapp-item-detail/src/main/java/local/example/seed/view/CustomerView.java
+++ webapp-item-detail/src/main/java/local/example/seed/view/CustomerView.java
@@ -19,6 +19,7 @@
 package local.example.seed.view;
 
 import com.vaadin.flow.component.AbstractField;
+import com.vaadin.flow.component.Unit;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.dependency.CssImport;
@@ -27,14 +28,18 @@ import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.grid.GridVariant;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Main;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.splitlayout.SplitLayout;
+import com.vaadin.flow.component.textfield.EmailField;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.binder.Binder;
+import com.vaadin.flow.data.binder.ValidationException;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import local.example.seed.layout.MainLayout;
 import local.example.seed.model.Customer;
+import local.example.seed.model.Link;
 import local.example.seed.service.CustomerRetrieverService;
 
 import java.util.Optional;
@@ -45,21 +50,20 @@ import java.util.Optional;
 public class CustomerView
         extends Main {
 
-    private Grid<Customer> customerGrid;
-    private Binder<Customer> customerBinder;
+    private final Grid<Customer> customerGrid;
+    private final Binder<Customer> customerBinder;
 
     private Customer customer;
-    private CustomerRetrieverService customerRetrieverService;
+    private final CustomerRetrieverService customerRetrieverService;
 
     private TextField name;
     private TextField surname;
-    private TextField email;
+    private EmailField email;
 
     private final Button cancel;
     private final Button update;
+    private final Button create;
     private final Button delete;
-
-    private final SplitLayout splitLayout;
 
     public CustomerView() {
         addClassName("main-view");
@@ -78,23 +82,84 @@ public class CustomerView
         });
         this.update = new Button("update");
         this.update.addClickListener(listener -> {
-            // TODO: behaviour
+            try {
+                if (this.customer != null) {
+                    this.customerBinder.writeBean(this.customer);
+                    this.customerRetrieverService.update(
+                            this.customer,
+                            this.customer.get_links().getSelf().getHref()
+                    );
+                    this.clear();
+                    this.refresh();
+                    this.reload();
+                    Notification.show("customer details have been updated");
+                }
+            } catch (ValidationException validationException) {
+                Notification.show("sorry, the customer details have not been updated");
+                validationException.printStackTrace();
+            }
+        });
+        this.create = new Button("create");
+        this.create.addClickListener(listener -> {
+            try {
+                if (
+                        !this.name.getValue().isEmpty() &
+                        !this.surname.getValue().isEmpty() &
+                        !this.email.getValue().isEmpty()
+                ) {
+                    this.customer = new Customer(
+                            this.name.getValue(),
+                            this.surname.getValue(),
+                            this.email.getValue(),
+                            new Link()
+                    );
+                    this.customerBinder.writeBean(this.customer);
+                    this.customerRetrieverService.create(
+                            this.customer
+                    );
+                    this.clear();
+                    this.refresh();
+                    this.reload();
+                    Notification.show("new customer's details have been created");
+                }
+            } catch (ValidationException validationException) {
+                Notification.show("sorry, the customer details have not been created");
+                validationException.printStackTrace();
+            }
         });
         this.delete = new Button("delete");
         this.delete.addClickListener(listener -> {
-            // TODO: behaviour
+            try {
+                if (this.customer != null) {
+                    this.customerBinder.writeBean(this.customer);
+                    this.customerRetrieverService.delete(
+                            this.customer.get_links().getSelf().getHref()
+                    );
+                    this.clear();
+                    this.refresh();
+                    this.reload();
+                    Notification.show("the selected customer has been deleted");
+                }
+            } catch (ValidationException validationException) {
+                Notification.show("sorry, the selected customer has not been deleted");
+                validationException.printStackTrace();
+            }
         });
 
-        this.splitLayout = new SplitLayout();
-        this.splitLayout.setSizeFull();
+        SplitLayout splitLayout = new SplitLayout();
+        splitLayout.setSizeFull();
 
-        this.customerGrid.setColumns("name", "surname", "email");
+        this.customerGrid.setColumns(
+                "name", "surname", "email"
+        );
         this.customerGrid.setItems(customerRetrieverService.readAll());
         this.customerGrid.addThemeVariants(GridVariant.LUMO_COLUMN_BORDERS);
-        this.customerGrid.setHeightFull();
+        this.customerGrid.setHeightByRows(true);
         this.customerGrid.asSingleSelect().addValueChangeListener(listener -> {
             if (listener.getValue() != null) {
-                Optional<Customer> customerFromBackend = customerRetrieverService.findByEmail(listener.getValue().getEmail());
+                Optional<Customer> customerFromBackend = this.customerRetrieverService.read(
+                        listener.getValue().get_links().getSelf().getHref()
+                );
                 if (customerFromBackend.isPresent()) {
                     this.populate(customerFromBackend.get());
                 } else {
@@ -107,7 +172,7 @@ public class CustomerView
 
         this.createGridLayout(splitLayout);
         this.createEditorLayout(splitLayout);
-        this.add(this.splitLayout);
+        this.add(splitLayout);
     }
 
     private void createEditorLayout(
@@ -117,6 +182,11 @@ public class CustomerView
         Div divEditor = new Div();
         divEditorLayout.add(divEditor);
         FormLayout formLayout = new FormLayout();
+        this.name.setAutofocus(true);
+        this.name.setMinWidth(100, Unit.PIXELS);
+        this.surname.setMinWidth(100, Unit.PIXELS);
+        this.email.setMinWidth(100, Unit.PIXELS);
+        this.email.setClearButtonVisible(true);
         addFormItem(divEditor, formLayout, this.name, "name");
         addFormItem(divEditor, formLayout, this.surname, "surname");
         addFormItem(divEditor, formLayout, this.email, "email");
@@ -132,8 +202,11 @@ public class CustomerView
         buttonHorizontalLayout.setSpacing(true);
         this.cancel.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
         this.update.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        this.create.addThemeVariants(ButtonVariant.LUMO_CONTRAST);
         this.delete.addThemeVariants(ButtonVariant.LUMO_ERROR);
-        buttonHorizontalLayout.add(this.cancel, this.update, this.delete);
+        buttonHorizontalLayout.add(
+                this.cancel, this.update, this.create, this.delete
+        );
         buttonHorizontalLayout.setSpacing(true);
         buttonHorizontalLayout.setMargin(true);
         divEditorLayout.add(buttonHorizontalLayout);
@@ -170,5 +243,9 @@ public class CustomerView
     private void populate(Customer customer) {
         this.customer = customer;
         this.customerBinder.readBean(this.customer);
+    }
+
+    private void reload() {
+        this.customerGrid.setItems(customerRetrieverService.readAll());
     }
 }
