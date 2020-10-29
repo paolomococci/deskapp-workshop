ab77502ff1f3af3fc86f42538f0b717984c158f6
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-10-29 10:17:55 +0100

    Update CustomerController.java

--- webapp-groovy-templates/src/main/java/local/example/seed/controller/CustomerController.java
+++ webapp-groovy-templates/src/main/java/local/example/seed/controller/CustomerController.java
@@ -18,11 +18,16 @@
 
 package local.example.seed.controller;
 
+import local.example.seed.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CustomerController {
+
+    @Autowired
+    private CustomerRepository customerRepository;
 
     @GetMapping("/customer")
     private String customer() {
