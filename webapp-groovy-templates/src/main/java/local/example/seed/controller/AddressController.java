ea384a50ccf96431ed4bd0c423efb40653db4a3f
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-10-29 10:17:06 +0100

    Update AddressController.java

--- webapp-groovy-templates/src/main/java/local/example/seed/controller/AddressController.java
+++ webapp-groovy-templates/src/main/java/local/example/seed/controller/AddressController.java
@@ -18,11 +18,16 @@
 
 package local.example.seed.controller;
 
+import local.example.seed.repository.AddressRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AddressController {
+
+    @Autowired
+    private AddressRepository addressRepository;
 
     @GetMapping("/address")
     private String address() {
