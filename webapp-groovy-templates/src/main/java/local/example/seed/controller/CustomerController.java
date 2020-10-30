08560982461ea7034a9e1a4b65c5da56ab0f01a1
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-10-30 08:45:25 +0100

    Update CustomerController.java

--- webapp-groovy-templates/src/main/java/local/example/seed/controller/CustomerController.java
+++ webapp-groovy-templates/src/main/java/local/example/seed/controller/CustomerController.java
@@ -18,10 +18,20 @@
 
 package local.example.seed.controller;
 
+import local.example.seed.document.Customer;
 import local.example.seed.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.sql.Timestamp;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class CustomerController {
@@ -29,8 +39,54 @@ public class CustomerController {
     @Autowired
     private CustomerRepository customerRepository;
 
+    @PostMapping("/customer-create")
+    public String createCustomer(@ModelAttribute Customer customer) {
+        this.customerRepository.save(customer);
+        return "redirect:/customer";
+    }
+
     @GetMapping("/customer")
-    private String customer() {
-        return "customer";
+    public ModelAndView customers() {
+        LinkedHashMap<String, List<Customer>> linkedHashMap;
+        linkedHashMap = new LinkedHashMap<>();
+        linkedHashMap.put("customers", this.customerRepository.findAll());
+        return new ModelAndView("customer", linkedHashMap);
+    }
+
+    @PostMapping("/customer-update")
+    public String updateCustomer(@ModelAttribute("customer") Customer customer) {
+        Optional<Customer> optionalCustomer;
+        try {
+            optionalCustomer = this.customerRepository.findById(customer.getId()).map(
+                    updatable -> {
+                        updatable.setName(customer.getName());
+                        updatable.setSurname(customer.getSurname());
+                        updatable.setEmail(customer.getEmail());
+                        this.customerRepository.save(updatable);
+                        return updatable;
+                    }
+            );
+            Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+            System.out.println(timestamp + " --- INFO --- Customer data has been correctly updated: " + optionalCustomer.toString());
+        } catch (NullPointerException nullPointerException) {
+            Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+            System.out.println(timestamp + " --- ERROR --- An error occurred while trying to update the customer data by ID: " + customer.getId());
+            nullPointerException.printStackTrace();
+        }
+        return "redirect:/customer";
+    }
+
+    @GetMapping("/customer-update/{id}")
+    public ModelAndView updateCustomerById(@PathVariable("id") String id) {
+        LinkedHashMap<String, Optional<Customer>> linkedHashMap;
+        linkedHashMap = new LinkedHashMap<>();
+        linkedHashMap.put("customer", this.customerRepository.findById(id));
+        return new ModelAndView("customer-update", linkedHashMap);
+    }
+
+    @GetMapping("/customer-delete/{id}")
+    public String deleteCustomer(@PathVariable("id") String id) {
+        this.customerRepository.deleteById(id);
+        return "redirect:/customer";
     }
 }
