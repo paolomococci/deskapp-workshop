87a63741b5173a91aa68d7dca1c128cbc02dac16
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-04 22:33:07 +0100

    Update HomeRouter.java

--- webapp-raw-flux/src/main/java/local/example/seed/router/HomeRouter.java
+++ webapp-raw-flux/src/main/java/local/example/seed/router/HomeRouter.java
@@ -18,5 +18,21 @@
 
 package local.example.seed.router;
 
+import local.example.seed.handler.HomeHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+@Configuration
 public class HomeRouter {
+
+    @Bean
+    public RouterFunction<ServerResponse> routeToRootPage(HomeHandler homeHandler) {
+        return RouterFunctions.route(RequestPredicates.GET("/")
+                .and(RequestPredicates.accept(MediaType.TEXT_HTML)), homeHandler::home);
+    }
 }
