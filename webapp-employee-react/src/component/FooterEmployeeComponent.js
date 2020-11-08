c5c52bca6a7252e93318520fb8912fccbd054d3b
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-08 17:20:26 +0100

    Create FooterEmployeeComponent.js

---
+++ webapp-employee-react/src/component/FooterEmployeeComponent.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+
+export default class FooterEmployeeComponent extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            
+        };
+    }
+
+    render() {
+        return (
+            <div>
+                <footer>
+                    <nav>
+                        <div>
+                            <p>employee data management</p>
+                        </div>
+                    </nav>
+                </footer>
+            </div>
+        )
+    }
+}
