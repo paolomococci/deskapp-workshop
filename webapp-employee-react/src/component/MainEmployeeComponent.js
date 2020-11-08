0a38f201ff858288468a2bd1f0af21779933ab53
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-08 17:20:00 +0100

    Create MainEmployeeComponent.js

---
+++ webapp-employee-react/src/component/MainEmployeeComponent.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+
+export default class MainEmployeeComponent extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            
+        };
+    }
+
+    render() {
+        return (
+            <div>
+                <main>
+                    <nav>
+                        <div>
+                            <p>TODO</p>
+                        </div>
+                    </nav>
+                </main>
+            </div>
+        )
+    }
+}
