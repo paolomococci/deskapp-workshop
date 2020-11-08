2bb3ce59e4006f25e41a4e6bde58bc25e36a3a27
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-08 18:58:14 +0100

    Update App.js

--- webapp-employee-react/src/App.js
+++ webapp-employee-react/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import HeaderEmployeeComponent from './component/HeaderEmployeeComponent';
+import MainEmployeeComponent from './component/MainEmployeeComponent';
+import FooterEmployeeComponent from './component/FooterEmployeeComponent';
 
 const App = () => {
   
   return (
     <div className="App">
       <HeaderEmployeeComponent/>
+      <MainEmployeeComponent/>
+      <FooterEmployeeComponent/>
     </div>
   );
 }
