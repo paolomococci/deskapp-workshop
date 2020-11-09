cd7e4e4cf25cdea2e09df0535794e07085711e60
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-09 18:55:28 +0100

    Update HeaderEmployeeComponent.js

--- webapp-employee-react/src/component/HeaderEmployeeComponent.js
+++ webapp-employee-react/src/component/HeaderEmployeeComponent.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import * as Icon from 'react-feather';
 
 export default class HeaderEmployeeComponent extends Component {
 
@@ -11,8 +12,14 @@ export default class HeaderEmployeeComponent extends Component {
     render() {
         return (
             <header>
-                <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+                <nav className="navbar navbar-expand-md navbar-dark bg-dark">
                     <h2 className="navbar-brand">employee data management</h2>
+                    <form className="form-inline my-2 my-lg-0">
+                        <input className="form-control mr-sm-2" type="text" placeholder="search" aria-label="search"/>
+                        <button className="btn btn-secondary my-2 my-sm-0" type="submit">
+                            <Icon.Search/> search
+                        </button>
+                    </form>
                 </nav>
             </header>
         )
