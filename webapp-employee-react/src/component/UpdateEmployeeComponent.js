9a204bd984f9d2f70c5ee3b81a1a82e0622b583c
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-09 19:02:56 +0100

    Create UpdateEmployeeComponent.js

---
+++ webapp-employee-react/src/component/UpdateEmployeeComponent.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+export default class UpdateEmployeeComponent extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            
+        };
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <span className="text-muted">TODO</span>
+            </div>
+        )
+    }
+}
