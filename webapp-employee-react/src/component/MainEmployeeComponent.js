329360a45827658907745f723966c8be6cd4b035
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-10 18:23:36 +0100

    Update MainEmployeeComponent.js

--- webapp-employee-react/src/component/MainEmployeeComponent.js
+++ webapp-employee-react/src/component/MainEmployeeComponent.js
@@ -18,6 +18,7 @@
 
 import React, {Component} from 'react';
 import * as Icon from 'react-feather';
+import EmployeeRetrieveService from '../service/EmployeeRetrieveService';
 
 export default class MainEmployeeComponent extends Component {
 
@@ -25,52 +26,83 @@ export default class MainEmployeeComponent extends Component {
         super(props);
 
         this.state = {
-            
+            employees: []
         };
+        this.addEmployee = this.addEmployee.bind(this);
+        this.editEmployee = this.editEmployee.bind(this);
+        this.deleteEmployee = this.deleteEmployee.bind(this);
+    }
+
+    addEmployee() {
+        // TODO
+    }
+
+    viewEmployee(id) {
+        // TODO
+    }
+
+    editEmployee(id) {
+        // TODO
+    }
+
+    deleteEmployee(id) {
+        // TODO
+    }
+
+    componentDidMount() {
+        // TODO
     }
 
     render() {
         return (
             <main className="col-md-9 ml-sm-auto col-lg-10 px-md-4">
                 <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-                    <h1 className="h1">employee</h1>
-                    <div ClassName="btn-toolbar mb-2 mb-md-0">
-                        <div ClassName="btn-group mr-2">
-                            <button type="button" ClassName="btn btn-sm btn-outline-secondary">
-                                <Icon.Plus/> add
+                    <h1 className="h1">employees</h1>
+                    <div className="btn-toolbar mb-2 mb-md-0">
+                        <div className="btn-group mr-2">
+                            <button type="button" className="btn btn-sm btn-outline-secondary">
+                                <Icon.Plus/>
                             </button>
                         </div>
                     </div>
                 </div>
-                <div ClassName="table-responsive">
-                    <table ClassName="table table-striped table-sm">
+                <div className="table-responsive">
+                    <table className="table table-striped table-sm">
                         <thead>
                             <tr>
                                 <th>name</th>
                                 <th>surname</th>
                                 <th>email</th>
                                 <th>profession</th>
-                                <th>actions</th>
+                                <th></th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td>someone</td>
-                                <td>someone</td>
-                                <td>someone</td>
-                                <td>someone</td>
-                                <td>
-                                    <button type="button" ClassName="btn btn-sm btn-outline-secondary">
-                                        <Icon.Upload/> update
-                                    </button>
-                                    <button type="button" ClassName="btn btn-sm btn-outline-secondary">
-                                        <Icon.Delete/> delete
-                                    </button>
-                                    <button type="button" ClassName="btn btn-sm btn-outline-secondary">
-                                        <Icon.Database/> view
-                                    </button>
-                                </td>
-                            </tr>
+                            {
+                                this.state.employees.map(
+                                    employee =>
+                                        <tr key={employee.id}>
+                                            <td>{employee.name}</td>
+                                            <td>{employee.surname}</td>
+                                            <td>{employee.email}</td>
+                                            <td>{employee.profession}</td>
+                                            <td>
+                                                <button onClick={()=>this.editEmployee(employee.id)} 
+                                                    type="button" className="btn btn-sm btn-outline-secondary">
+                                                    <Icon.Upload/>
+                                                </button>
+                                                <button onClick={()=>this.deleteEmployee(employee.id)} 
+                                                    type="button" className="btn btn-sm btn-outline-secondary">
+                                                    <Icon.Delete/>
+                                                </button>
+                                                <button onClick={()=>this.viewEmployee(employee.id)} 
+                                                    type="button" className="btn btn-sm btn-outline-secondary">
+                                                    <Icon.Database/>
+                                                </button>
+                                            </td>
+                                        </tr>
+                                )
+                            }
                         </tbody>
                         <tfoot>
                             <tr>
