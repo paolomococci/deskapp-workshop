7c66dde38b003e7a7b42fcf43859ff49ab49034e
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-15 22:12:21 +0100

    Create App.vue

---
+++ first-project/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+	<v-app>
+		<v-app-bar app color="#8c9" dark>
+			<div class="d-flex align-center">
+				<h2>first project</h2>
+			</div>
+			<v-spacer></v-spacer>
+			<v-btn href="#" target="_blank" text>
+				<span class="mr-2">help</span>
+				<v-icon>mdi-open-in-new</v-icon>
+			</v-btn>
+		</v-app-bar>
+		<v-main>
+			<pm-data-table></pm-data-table>
+		</v-main>
+	</v-app>
+</template>
+
+<script>
+import DataTableComponent from './components/DataTableComponent';
+
+export default {
+	name: 'App',
+
+	components: {
+		'pm-data-table': DataTableComponent
+	},
+
+	data: () => ({
+		// TODO
+	})
+};
+</script>
+
