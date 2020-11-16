e3cd7ed806e5277937aac01541be403d7ff1f3a4
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-16 08:02:16 +0100

    Create About.vue

---
+++ second-project/src/views/About.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="about">
+    <welcome titleOfPage="about page of this second project"/>
+  </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+import WelcomeComponent from '@/components/WelcomeComponent';
+
+export default {
+  name: 'Home',
+  components: {
+    'welcome': WelcomeComponent
+  }
+}
+</script>
