3a8555de43ee959c4e42808264a1f565a8442dcd
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-16 08:04:50 +0100

    Create Home.vue

---
+++ second-project/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <welcome titleOfPage="welcome to this example of Vue.js frontend web application"/>
+  </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+import WelcomeComponent from '@/components/WelcomeComponent';
+
+export default {
+  name: 'Home',
+  components: {
+    'welcome': WelcomeComponent
+  }
+}
+</script>
