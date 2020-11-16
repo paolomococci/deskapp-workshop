3d01611fb9e5e8a6a408e6af47cdee849d37546e
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-16 08:08:38 +0100

    Create WelcomeComponent.vue

---
+++ second-project/src/components/WelcomeComponent.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="hello">
+    <h1>{{titleOfPage}}</h1>
+  </div>
+</template>
+
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #4b8;
+}
+</style>
+
+<script>
+export default {
+  name: 'WelcomeComponent',
+  props: {
+    titleOfPage: String
+  }
+}
+</script>
