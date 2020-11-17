f2e92a608bd4d0e95f47588aa829941036c6e40e
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-17 20:10:10 +0100

    Create FormExampleComponent.vue

---
+++ second-project/src/components/FormExampleComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <!-- TODO -->
+  </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+export default {
+  name: FormExampleComponent,
+  data: () => ({
+    // TODO
+  }),
+  methods: {
+    // TODO
+  },
+  computed: {
+    // TODO
+  }
+}
+</script>
