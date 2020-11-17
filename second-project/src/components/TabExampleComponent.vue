f31bc4c0b8b703820cd3dc8938c5ef41b695e937
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-17 20:12:23 +0100

    Create TabExampleComponent.vue

---
+++ second-project/src/components/TabExampleComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <!-- TODO -->
+  </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+export default {
+  name: TabExampleComponent,
+  data: () => ({
+    // TODO
+  }),
+  methods: {
+    // TODO
+  },
+  computed: {
+    // TODO
+  }
+}
+</script>
