12071caea34a4fbb2304aa5ba1fb01d0dfbbf526
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-17 20:08:45 +0100

    Create CalendarExampleComponent.vue

---
+++ second-project/src/components/CalendarExampleComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <!-- TODO -->
+  </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+export default {
+  name: CalendarExampleComponent,
+  data: () => ({
+    // TODO
+  }),
+  methods: {
+    // TODO
+  },
+  computed: {
+    // TODO
+  }
+}
+</script>
