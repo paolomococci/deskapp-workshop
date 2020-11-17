4e0107cc82d23d0aa43a7d929651b58d1d9352d7
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-17 20:05:58 +0100

    Create AvatarExampleComponent.vue

---
+++ second-project/src/components/AvatarExampleComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <!-- TODO -->
+  </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+export default {
+  name: AlertExampleComponent,
+  data: () => ({
+    // TODO
+  }),
+  methods: {
+    // TODO
+  },
+  computed: {
+    // TODO
+  }
+}
+</script>
