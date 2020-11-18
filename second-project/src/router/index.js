a3172ae50c8f9c5c600df0002a14a861a9f163b1
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-18 20:45:50 +0100

    Update index.js

--- second-project/src/router/index.js
+++ second-project/src/router/index.js
@@ -14,6 +14,11 @@ const routes = [
     path: '/about',
     name: 'About',
     component: () => import('../views/About.vue')
+  },
+  {
+    path: '/navbar',
+    name: 'Navbar',
+    component: () => import('../views/Navbar.vue')
   }
 ]
 
