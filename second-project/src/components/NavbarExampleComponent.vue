07d7f66dbeee8b46ef1679c006211b00d0dd4388
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-18 17:53:14 +0100

    Create NavbarExampleComponent.vue

---
+++ second-project/src/components/NavbarExampleComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <!-- TODO -->
+  </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+export default {
+  name: NavbarExampleComponent,
+  data: () => ({
+    // TODO
+  }),
+  methods: {
+    // TODO
+  },
+  computed: {
+    // TODO
+  }
+}
+</script>
