654d604f3c27e27b645335198ebc49c7c1a02564
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-18 17:52:17 +0100

    Create NavExampleComponent.vue

---
+++ second-project/src/components/NavExampleComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <!-- TODO -->
+  </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+export default {
+  name: NavExampleComponent,
+  data: () => ({
+    // TODO
+  }),
+  methods: {
+    // TODO
+  },
+  computed: {
+    // TODO
+  }
+}
+</script>
