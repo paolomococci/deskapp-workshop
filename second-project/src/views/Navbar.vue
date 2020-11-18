dc4abb1e262d4f3a90505efd02c66830fb7b959f
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-18 20:47:13 +0100

    Create Navbar.vue

---
+++ second-project/src/views/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <welcome titleOfPage="navbar todo"/>
+  </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+import WelcomeComponent from '@/components/WelcomeComponent'
+
+export default {
+  name: 'Navbar',
+  components: {
+    welcome: WelcomeComponent
+  }
+}
+</script>
