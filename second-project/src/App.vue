d3f0602ff7893dbc1ac8a5ccba9ad08b4a2c700d
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-18 20:45:16 +0100

    Update App.vue

--- second-project/src/App.vue
+++ second-project/src/App.vue
@@ -1,8 +1,11 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> 
-      <router-link to="/about">About</router-link>
+      <router-link to="/">home</router-link>
+      <span> &#173; </span>
+      <router-link to="/about">about</router-link>
+      <span> &#173; </span>
+      <router-link to="/navbar">navbar</router-link>
     </div>
     <router-view/>
   </div>
