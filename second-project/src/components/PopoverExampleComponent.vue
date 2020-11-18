724d6e376d0334b265de9f2ae319e8c15dcdd33c
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-18 17:54:21 +0100

    Create PopoverExampleComponent.vue

---
+++ second-project/src/components/PopoverExampleComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <!-- TODO -->
+  </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+export default {
+  name: PopoverExampleComponent,
+  data: () => ({
+    // TODO
+  }),
+  methods: {
+    // TODO
+  },
+  computed: {
+    // TODO
+  }
+}
+</script>
