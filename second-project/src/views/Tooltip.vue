db624ed04dae86693a027d9cbb668c887345a154
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-19 17:31:28 +0100

    Create Tooltip.vue

---
+++ second-project/src/views/Tooltip.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <welcome titleOfPage="tooltip todo"/>
+  </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+import WelcomeComponent from '@/components/WelcomeComponent'
+
+export default {
+  name: 'Tooltip',
+  components: {
+    welcome: WelcomeComponent
+  }
+}
+</script>
