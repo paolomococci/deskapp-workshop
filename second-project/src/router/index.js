fb868acfbcd7cb6600ed0f42b154300fbaffcb5a
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-19 17:45:20 +0100

    Update index.js

--- second-project/src/router/index.js
+++ second-project/src/router/index.js
@@ -21,6 +21,36 @@ const routes = [
     component: () => import('../views/Alert.vue')
   },
   {
+    path: '/avatar',
+    name: 'Avatar',
+    component: () => import('../views/Avatar.vue')
+  },
+  {
+    path: '/badge',
+    name: 'Badge',
+    component: () => import('../views/Badge.vue')
+  },
+  {
+    path: '/breadcrumb',
+    name: 'Breadcrumb',
+    component: () => import('../views/Breadcrumb.vue')
+  },
+  {
+    path: '/button',
+    name: 'Button',
+    component: () => import('../views/Button.vue')
+  },
+  {
+    path: '/calendar',
+    name: 'Calendar',
+    component: () => import('../views/Calendar.vue')
+  },
+  {
+    path: '/card',
+    name: 'Card',
+    component: () => import('../views/Card.vue')
+  },
+  {
     path: '/navbar',
     name: 'Navbar',
     component: () => import('../views/Navbar.vue')
