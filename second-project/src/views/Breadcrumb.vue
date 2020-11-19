f0a1a26cb5b347b135fd486c06f9b18e4d74c5b2
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-19 17:10:55 +0100

    Create Breadcrumb.vue

---
+++ second-project/src/views/Breadcrumb.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <welcome titleOfPage="breadcrumb todo"/>
+  </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+import WelcomeComponent from '@/components/WelcomeComponent'
+
+export default {
+  name: 'Breadcrumb',
+  components: {
+    welcome: WelcomeComponent
+  }
+}
+</script>
