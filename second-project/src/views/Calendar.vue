8ac7e13b00afcb49e0a29f3db8e034326234caf7
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-19 17:10:12 +0100

    Create Calendar.vue

---
+++ second-project/src/views/Calendar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <welcome titleOfPage="calendar todo"/>
+  </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+import WelcomeComponent from '@/components/WelcomeComponent'
+
+export default {
+  name: 'Calendar',
+  components: {
+    welcome: WelcomeComponent
+  }
+}
+</script>
