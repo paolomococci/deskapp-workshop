1d2810089797fead11650333a126ac409e92b3ba
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-19 16:55:50 +0100

    Create Preference.vue

---
+++ second-project/src/views/Preference.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <preference/>
+  </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+import PreferenceComponent from '@/components/PreferenceComponent'
+
+export default {
+  name: 'Preference',
+  components: {
+    preference: PreferenceComponent
+  }
+}
+</script>
