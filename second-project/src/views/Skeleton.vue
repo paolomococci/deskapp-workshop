b1c0b06f5964a413937696d064036e7ddc37fb03
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-19 17:23:17 +0100

    Create Skeleton.vue

---
+++ second-project/src/views/Skeleton.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <welcome titleOfPage="skeleton todo"/>
+  </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+import WelcomeComponent from '@/components/WelcomeComponent'
+
+export default {
+  name: 'Skeleton',
+  components: {
+    welcome: WelcomeComponent
+  }
+}
+</script>
