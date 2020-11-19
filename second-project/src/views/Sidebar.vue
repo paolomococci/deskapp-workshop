ff758c7c9f1adf7453cc23e5ab0d20efe9e8920d
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-19 17:23:49 +0100

    Create Sidebar.vue

---
+++ second-project/src/views/Sidebar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <welcome titleOfPage="sidebar todo"/>
+  </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+import WelcomeComponent from '@/components/WelcomeComponent'
+
+export default {
+  name: 'Sidebar',
+  components: {
+    welcome: WelcomeComponent
+  }
+}
+</script>
