68d874dffc0d379a080ba13acbdb7c8fd66691d8
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-19 16:57:50 +0100

    Update PreferenceComponent.vue

--- second-project/src/components/PreferenceComponent.vue
+++ second-project/src/components/PreferenceComponent.vue
@@ -1,1 +1,24 @@
+<template>
+  <div>
+    <!-- TODO -->
+  </div>
+</template>
 
+<style>
+
+</style>
+
+<script>
+export default {
+  name: 'PreferenceComponent',
+  data: () => ({
+    // TODO
+  }),
+  methods: {
+    // TODO
+  },
+  computed: {
+    // TODO
+  }
+}
+</script>
