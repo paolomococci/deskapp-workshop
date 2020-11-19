50ce50a2c9b588d97f4429543c562af8bfdf611b
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-19 16:52:05 +0100

    Create Avatar.vue

---
+++ second-project/src/views/Avatar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <welcome titleOfPage="avatar todo"/>
+  </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+import WelcomeComponent from '@/components/WelcomeComponent'
+
+export default {
+  name: 'Avatar',
+  components: {
+    welcome: WelcomeComponent
+  }
+}
+</script>
