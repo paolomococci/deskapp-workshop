01a710cbe69516b415d62093ba3c1a3d67d04ff1
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-19 17:33:02 +0100

    Create Table.vue

---
+++ second-project/src/views/Table.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <welcome titleOfPage="table todo"/>
+  </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+import WelcomeComponent from '@/components/WelcomeComponent'
+
+export default {
+  name: 'Table',
+  components: {
+    welcome: WelcomeComponent
+  }
+}
+</script>
