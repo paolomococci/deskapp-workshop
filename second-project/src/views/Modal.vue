e59f9b50a9791295cfd47751cd4f575d371b5bc5
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-19 17:26:34 +0100

    Create Modal.vue

---
+++ second-project/src/views/Modal.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <welcome titleOfPage="modal todo"/>
+  </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+import WelcomeComponent from '@/components/WelcomeComponent'
+
+export default {
+  name: 'Modal',
+  components: {
+    welcome: WelcomeComponent
+  }
+}
+</script>
