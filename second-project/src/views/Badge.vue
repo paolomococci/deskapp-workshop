5229f69faedf03ee992ab1f282ecc9288f126cde
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-19 17:11:21 +0100

    Create Badge.vue

---
+++ second-project/src/views/Badge.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <welcome titleOfPage="badge todo"/>
+  </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+import WelcomeComponent from '@/components/WelcomeComponent'
+
+export default {
+  name: 'Badge',
+  components: {
+    welcome: WelcomeComponent
+  }
+}
+</script>
