6b4a169074fb8edf99a998b7660b33717b8968f9
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-19 17:09:10 +0100

    Create Form.vue

---
+++ second-project/src/views/Form.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <welcome titleOfPage="form todo"/>
+  </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+import WelcomeComponent from '@/components/WelcomeComponent'
+
+export default {
+  name: 'Form',
+  components: {
+    welcome: WelcomeComponent
+  }
+}
+</script>
