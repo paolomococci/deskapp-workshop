105d985160511e84994c0c3c33794dac6f87c159
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-19 17:32:02 +0100

    Create Toast.vue

---
+++ second-project/src/views/Toast.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <welcome titleOfPage="toast todo"/>
+  </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+import WelcomeComponent from '@/components/WelcomeComponent'
+
+export default {
+  name: 'Toast',
+  components: {
+    welcome: WelcomeComponent
+  }
+}
+</script>
