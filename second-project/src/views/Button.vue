49ccdf4b39b9e9498ed56d605cd95bf4ae362657
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-19 17:09:45 +0100

    Create Button.vue

---
+++ second-project/src/views/Button.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <welcome titleOfPage="button todo"/>
+  </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+import WelcomeComponent from '@/components/WelcomeComponent'
+
+export default {
+  name: 'Button',
+  components: {
+    welcome: WelcomeComponent
+  }
+}
+</script>
