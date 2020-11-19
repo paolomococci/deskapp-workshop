1514840946dbe1a8d003a1d71d30f6bbbb4bd2c4
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-19 16:52:31 +0100

    Create Alert.vue

---
+++ second-project/src/views/Alert.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <welcome titleOfPage="alert todo"/>
+  </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+import WelcomeComponent from '@/components/WelcomeComponent'
+
+export default {
+  name: 'Alert',
+  components: {
+    welcome: WelcomeComponent
+  }
+}
+</script>
