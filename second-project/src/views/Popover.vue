2e5427b438be6901e34a82bcc2912f86d19f2d61
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-19 17:24:24 +0100

    Create Popover.vue

---
+++ second-project/src/views/Popover.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <welcome titleOfPage="popover todo"/>
+  </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+import WelcomeComponent from '@/components/WelcomeComponent'
+
+export default {
+  name: 'Popover',
+  components: {
+    welcome: WelcomeComponent
+  }
+}
+</script>
