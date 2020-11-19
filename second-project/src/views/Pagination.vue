63c3ff400c3a8eb551dfee92e3e451ef38fec719
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-19 17:25:38 +0100

    Create Pagination.vue

---
+++ second-project/src/views/Pagination.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <welcome titleOfPage="pagination todo"/>
+  </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+import WelcomeComponent from '@/components/WelcomeComponent'
+
+export default {
+  name: 'Pagination',
+  components: {
+    welcome: WelcomeComponent
+  }
+}
+</script>
