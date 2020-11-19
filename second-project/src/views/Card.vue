cb1385657759cf0d550840ce56dc0fb6c17d4c36
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-19 17:08:34 +0100

    Create Card.vue

---
+++ second-project/src/views/Card.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <welcome titleOfPage="card todo"/>
+  </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+import WelcomeComponent from '@/components/WelcomeComponent'
+
+export default {
+  name: 'Card',
+  components: {
+    welcome: WelcomeComponent
+  }
+}
+</script>
