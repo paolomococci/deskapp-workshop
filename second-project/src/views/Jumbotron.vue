efc571414c8f4282e38de12bd284de2f1d958f74
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-19 17:08:05 +0100

    Create Jumbotron.vue

---
+++ second-project/src/views/Jumbotron.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <welcome titleOfPage="jumbotron todo"/>
+  </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+import WelcomeComponent from '@/components/WelcomeComponent'
+
+export default {
+  name: 'Jumbotron',
+  components: {
+    welcome: WelcomeComponent
+  }
+}
+</script>
