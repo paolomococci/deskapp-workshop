d51557917efd123937630107ac90a81ee7943a80
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-19 17:26:03 +0100

    Create Nav.vue

---
+++ second-project/src/views/Nav.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <welcome titleOfPage="nav todo"/>
+  </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+import WelcomeComponent from '@/components/WelcomeComponent'
+
+export default {
+  name: 'Nav',
+  components: {
+    welcome: WelcomeComponent
+  }
+}
+</script>
