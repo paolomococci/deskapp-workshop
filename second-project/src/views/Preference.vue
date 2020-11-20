cb88429223fd2c4198f52a952374dabdd11bd172
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-20 19:06:03 +0100

    Update Preference.vue

--- second-project/src/views/Preference.vue
+++ second-project/src/views/Preference.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <welcome titleOfPage="statistical survey example"/>
     <preference/>
   </div>
 </template>
@@ -9,11 +10,13 @@
 </style>
 
 <script>
+import WelcomeComponent from '@/components/WelcomeComponent'
 import PreferenceComponent from '@/components/PreferenceComponent'
 
 export default {
   name: 'Preference',
   components: {
+    welcome: WelcomeComponent,
     preference: PreferenceComponent
   }
 }
