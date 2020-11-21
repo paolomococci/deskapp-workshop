1a4bdbcaec76419d459b8fa91d7f90dde4e47851
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-21 11:03:38 +0100

    Update CardExampleComponent.vue

--- second-project/src/components/CardExampleComponent.vue
+++ second-project/src/components/CardExampleComponent.vue
@@ -1,6 +1,26 @@
 <template>
   <div>
-    <!-- TODO -->
+    <b-card-group deck>
+      <b-card
+        header="some header"
+        header-tag="header"
+        footer="some footer"
+        footer-tag="footer"
+        title="some title">
+        <b-card-text>example of simply text</b-card-text>
+        <b-button href="#" variant="primary">somewhere</b-button>
+      </b-card>
+      <b-card title="some title" header-tag="header" footer-tag="footer">
+        <template #header>
+          <h6 class="mb-0">header</h6>
+        </template>
+        <b-card-text>example of text content</b-card-text>
+        <b-button href="#" variant="primary">somewhere</b-button>
+        <template #footer>
+          <em>footer</em>
+        </template>
+      </b-card>
+    </b-card-group>
   </div>
 </template>
 
@@ -10,15 +30,6 @@
 
 <script>
 export default {
-  name: 'CardExampleComponent',
-  data: () => ({
-    // TODO
-  }),
-  methods: {
-    // TODO
-  },
-  computed: {
-    // TODO
-  }
+  name: 'CardExampleComponent'
 }
 </script>
