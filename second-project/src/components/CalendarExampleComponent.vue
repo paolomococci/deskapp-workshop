51bbdbac051efadceb9c12b78d9d1289f1597fe3
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-21 11:03:05 +0100

    Update CalendarExampleComponent.vue

--- second-project/src/components/CalendarExampleComponent.vue
+++ second-project/src/components/CalendarExampleComponent.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
-    <!-- TODO -->
+    <b-row>
+      <b-col md="auto">
+        <b-calendar v-model="value" @context="onContext" locale="it-IT"></b-calendar>
+      </b-col>
+      <b-col>
+        <p>value: <em>{{value}}</em></p>
+        <p class="mb-0">context</p>
+        <pre class="small">{{context}}</pre>
+      </b-col>
+    </b-row>
   </div>
 </template>
 
@@ -12,13 +21,13 @@
 export default {
   name: 'CalendarExampleComponent',
   data: () => ({
-    // TODO
+    value: '',
+    context: null
   }),
   methods: {
-    // TODO
-  },
-  computed: {
-    // TODO
+    onContext (contextOfCalendar) {
+      this.context = contextOfCalendar
+    }
   }
 }
 </script>
