ea318803cfca9b098e806bd0b11b5cf602b56409
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-21 11:05:03 +0100

    Update Card.vue

--- second-project/src/views/Card.vue
+++ second-project/src/views/Card.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <welcome titleOfPage="card todo"/>
+    <welcome titleOfPage="example of cards"/>
+    <exampleOfCard/>
   </div>
 </template>
 
@@ -10,11 +11,13 @@
 
 <script>
 import WelcomeComponent from '@/components/WelcomeComponent'
+import CardExampleComponent from '@/components/CardExampleComponent'
 
 export default {
   name: 'Card',
   components: {
-    welcome: WelcomeComponent
+    welcome: WelcomeComponent,
+    exampleOfCard: CardExampleComponent
   }
 }
 </script>
