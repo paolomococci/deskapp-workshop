22b1806c31a312772a1eee8bb352185dac5f3567
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-22 09:54:24 +0100

    Update ToastExampleComponent.vue

--- second-project/src/components/ToastExampleComponent.vue
+++ second-project/src/components/ToastExampleComponent.vue
@@ -1,6 +1,18 @@
 <template>
   <div>
-    <!-- TODO -->
+    <b-button @click="showToast()" class="mb-2">default variant</b-button>
+    <br/>
+    <b-button variant="primary" @click="showToast('primary')" class="mb-2">primary variant</b-button>
+    <br/>
+    <b-button variant="secondary" @click="showToast('secondary')" class="mb-2">secondary variant</b-button>
+    <br/>
+    <b-button variant="danger" @click="showToast('danger')" class="mb-2">danger variant</b-button>
+    <br/>
+    <b-button variant="warning" @click="showToast('warning')" class="mb-2">warning variant</b-button>
+    <br/>
+    <b-button variant="success" @click="showToast('success')" class="mb-2">success variant</b-button>
+    <br/>
+    <b-button variant="info" @click="showToast('info')" class="mb-2">info variant</b-button>
   </div>
 </template>
 
@@ -15,7 +27,13 @@ export default {
     // TODO
   }),
   methods: {
-    // TODO
+    showToast (variant = null) {
+      this.$bvToast.toast('some content of this variant component', {
+        title: `tast example with variant: ${variant || 'default'}`,
+        variant: variant,
+        solid: true
+      })
+    }
   },
   computed: {
     // TODO
