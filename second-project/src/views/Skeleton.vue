d5a5c35727dfae856b4d7efc5a728f37c891440b
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-22 09:59:23 +0100

    Update Skeleton.vue

--- second-project/src/views/Skeleton.vue
+++ second-project/src/views/Skeleton.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <welcome titleOfPage="skeleton todo"/>
+    <welcome titleOfPage="example of skeleton"/>
+    <exampleOfSkeleton/>
   </div>
 </template>
 
@@ -10,11 +11,13 @@
 
 <script>
 import WelcomeComponent from '@/components/WelcomeComponent'
+import SkeletonExampleComponent from '@/components/SkeletonExampleComponent'
 
 export default {
   name: 'Skeleton',
   components: {
-    welcome: WelcomeComponent
+    welcome: WelcomeComponent,
+    exampleOfSkeleton: SkeletonExampleComponent
   }
 }
 </script>
