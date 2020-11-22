f3acd8443072c1a726ac98e3cf757cf84b4bdca7
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-22 09:42:29 +0100

    Update ModalExampleComponent.vue

--- second-project/src/components/ModalExampleComponent.vue
+++ second-project/src/components/ModalExampleComponent.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <!-- TODO -->
+   <b-button v-b-modal.modal-01>example of modal</b-button>
+    <b-modal id="modal-01" title="simply modal">
+      <p class="my-4">this is a simply example of modal</p>
+    </b-modal>
   </div>
 </template>
 
