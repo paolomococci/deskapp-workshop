c6610476e02e41dc2c3f27d8a413a465850825a5
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-22 09:40:06 +0100

    Update BadgeExampleComponent.vue

--- second-project/src/components/BadgeExampleComponent.vue
+++ second-project/src/components/BadgeExampleComponent.vue
@@ -1,6 +1,20 @@
 <template>
   <div>
-    <!-- TODO -->
+    <b-badge href="#" variant="primary">example of primary variant</b-badge>
+    <br/>
+    <b-badge href="#" variant="secondary">example of secondary variant</b-badge>
+    <br/>
+    <b-badge href="#" variant="success">example of success variant</b-badge>
+    <br/>
+    <b-badge href="#" variant="danger">example of danger variant</b-badge>
+    <br/>
+    <b-badge href="#" variant="warning">example of warning variant</b-badge>
+    <br/>
+    <b-badge href="#" variant="info">example of info variant</b-badge>
+    <br/>
+    <b-badge href="#" variant="light">example of light variant</b-badge>
+    <br/>
+    <b-badge href="#" variant="dark">example of dark variant</b-badge>
   </div>
 </template>
 
@@ -10,15 +24,6 @@
 
 <script>
 export default {
-  name: 'BadgeExampleComponent',
-  data: () => ({
-    // TODO
-  }),
-  methods: {
-    // TODO
-  },
-  computed: {
-    // TODO
-  }
+  name: 'BadgeExampleComponent'
 }
 </script>
