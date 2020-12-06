e77004fc1f84b7fe1b60f59cf9cd9313a044c459
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-06 22:36:04 +0100

    Update MainLayout.java

--- webapp-item-detail/src/main/java/local/example/seed/layout/MainLayout.java
+++ webapp-item-detail/src/main/java/local/example/seed/layout/MainLayout.java
@@ -32,14 +32,11 @@ import com.vaadin.flow.component.tabs.Tabs;
 import com.vaadin.flow.component.tabs.TabsVariant;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.RouterLink;
-import com.vaadin.flow.theme.Theme;
-import com.vaadin.flow.theme.lumo.Lumo;
 import local.example.seed.view.*;
 
 import java.util.Optional;
 
 @CssImport("style.css")
-@Theme(value = Lumo.class, variant = Lumo.DARK)
 public class MainLayout
         extends AppLayout {
 
