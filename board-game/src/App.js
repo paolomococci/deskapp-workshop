a54c57bf31e5b2479a0fa5d1b71df944c9dd4d00
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-03-05 17:47:11 +0100

    Update App.js

--- board-game/src/App.js
+++ board-game/src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
       <header className="App-header">
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
       </header>
     </div>
-  );
+    );
+  }
 }
-
-export default App;
