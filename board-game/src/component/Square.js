39ed6b72e08c3f41e306db78349d8e9978df9d42
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-03-05 17:05:14 +0100

    Update Square.js

--- board-game/src/component/Square.js
+++ board-game/src/component/Square.js
@@ -1,6 +1,15 @@
 import React from "react";
 
 export default class Square extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: "clickable"
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
   render() {
     return (
       <button className="square">
