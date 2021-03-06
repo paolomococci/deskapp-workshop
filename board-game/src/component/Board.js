be8248654b4d98e3d37a5da30ce50c85478159f8
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-03-06 15:14:28 +0100

    Update Board.js

--- board-game/src/component/Board.js
+++ board-game/src/component/Board.js
@@ -1,31 +1,31 @@
 import React from "react";
-import Square from './Square';
+import Box from './Box';
 
 export default class Board extends React.Component {
-  renderSquare(arg) {
-    return <Square value={arg}/>;
+  renderBox(arg) {
+    return <Box value={arg}/>;
   }
   
   render() {
-    const status = 'player: X';
+    const status = 'Player: X';
 
     return (
       <div>
         <div className="App-status">{status}</div>
         <div className="App-board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
+          {this.renderBox(0)}
+          {this.renderBox(1)}
+          {this.renderBox(2)}
         </div>
         <div className="App-board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
+          {this.renderBox(3)}
+          {this.renderBox(4)}
+          {this.renderBox(5)}
         </div>
         <div className="App-board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
+          {this.renderBox(6)}
+          {this.renderBox(7)}
+          {this.renderBox(8)}
         </div>
       </div>
     );
