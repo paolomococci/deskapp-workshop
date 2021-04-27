ae6851b1c899f8be609ba68dfc57f18ee38bb0e6
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-04-27 22:33:36 +0200

    Update build.gradle.kts

--- webapp-empty-kotlin/build.gradle.kts
+++ webapp-empty-kotlin/build.gradle.kts
@@ -15,7 +15,7 @@ repositories {
 	mavenCentral()
 }
 
-extra["vaadinVersion"] = "19.0.5"
+extra["vaadinVersion"] = "19.0.6"
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-hateoas")
