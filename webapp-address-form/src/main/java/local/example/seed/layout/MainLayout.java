62dfc0f52fce91a7d7f947cdba6fb7621fc4053b
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-27 13:34:54 +0200

    Update MainLayout.java

--- webapp-address-form/src/main/java/local/example/seed/layout/MainLayout.java
+++ webapp-address-form/src/main/java/local/example/seed/layout/MainLayout.java
@@ -22,7 +22,6 @@ import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.ComponentUtil;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.applayout.DrawerToggle;
-import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
@@ -39,7 +38,6 @@ import local.example.seed.view.MainView;
 
 import java.util.Optional;
 
-@CssImport("style.css")
 public class MainLayout
         extends AppLayout {
 
